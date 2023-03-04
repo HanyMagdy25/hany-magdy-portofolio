e4ed9241f04497dfcd8dba8d005c5c7e0e811b3c
HanyMagdy25 <hany.magdy25@gmail.com>
2023-03-04 02:35:18 +0200

    canavs commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
+import { BrowserRouter } from "react-router-dom";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
 
 function App() {
-
-
   return (
-    <div className="App">
-     Hany Magdy not 3d
-    </div>
-  )
+    <BrowserRouter>
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <Navbar />
+          <Hero />
+        </div>
+        {/* <About /> */}
+        {/* <Experience /> */}
+        {/* <Tech /> */}
+        {/* <Works /> */}
+        {/* <Feedbacks /> */}
+        {/* <div className='relative z-0'>
+          <Contact />
+          <StarsCanvas />
+        </div> */}
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
